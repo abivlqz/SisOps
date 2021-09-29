54876c3502663332d21f8b74e7ae921483384839
Abigail Velazquez <aby1907@hotmail.com>
2021-09-29 12:55:35 -0500

    Initial Commit

---
+++ SisOps/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SisOps
+//
+//  Created by Aby Velazquez on 29/09/21.
+//  Copyright © 2021 Abi Velazquez. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
